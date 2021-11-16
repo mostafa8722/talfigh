029e840668b38268bee82b10b6977470c23c4055
Alireza Hosseini <alireza.hosseini7902@gmail.com>
2021-11-16 17:46:34 +0330

    initial && start users page

---
+++ pages/users/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue'
+
+export default  Vue.extend({
+  name: 'Index',
+  data(){
+    return {
+
+    }
+  }
+})
+</script>
+
+<style scoped lang='scss'>
+
+</style>
