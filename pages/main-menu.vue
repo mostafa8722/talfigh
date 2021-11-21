e2ccfda1a9a4c20ffea079d4cd1473f691ff1547
hosseindanesh7 <daneshfar.hossein7@gmail.com>
2021-11-21 16:36:14 +0330

    main menu done

--- pages/main-menu.vue
+++ pages/main-menu.vue
@@ -1,21 +1,55 @@
 <template>
   <div class="pa-md-10 pa-2">
-    <v-card class="pa-6 rounded-lg"
-      ><span style="color: #747474;font-size: 1.6rem">جزئیات منو</span>
+    <div class="mb-4" style="color: #fff; font-size: 1.6rem">منو وبسایت</div>
+    <v-card style="background-color: #e3e3e3" class="pa-8 rounded-lg mb-6">
+      <v-row class="mb-2">
+        <v-col cols="12" lg="4" md="4">
+          <span style="color: #747474">عنوان</span>
+          <v-text-field background-color="white" rounded></v-text-field>
+        </v-col>
+        <v-col cols="12" lg="4" md="4">
+          <span style="color: #747474">لینک خارجی</span>
+          <v-text-field background-color="white" rounded></v-text-field>
+        </v-col>
+        <v-col cols="12" lg="4" md="4">
+          <v-btn class="px-10 mt-8 rounded-xl" color="primary">
+            <v-icon size="10">fas fa-plus</v-icon>&nbsp; اضافه کردن منو به لیست
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-card>
+    <v-card class="pa-6 rounded-lg">
+      <span style="color: #747474; font-size: 1.2rem">ترتیب منو</span>
       <div class="simple-page mt-4">
         <Container @drop="onDrop($event)">
-          <Draggable v-for="(item, i) in items" :key="item.id">
+          <Draggable v-for="item in items" :key="item.id">
             <div class="draggable-item">
-              <v-expansion-panels>
-                <v-expansion-panel>
+              <v-expansion-panels flat>
+                <v-expansion-panel class="mb-2">
                   <v-expansion-panel-header>
-                    Item {{ i }}
+                    <span class="expansion-header-title">
+                      <v-icon color="#D9D9D9" class="ml-4">
+                        fa-align-justify</v-icon
+                      >
+                      {{ item.data }}
+                    </span>
                   </v-expansion-panel-header>
                   <v-expansion-panel-content>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    <v-row>
+                      <v-col class="mt-4" cols="12">
+                        <span style="color: #747474">عنوان</span>
+                        <v-text-field rounded dense></v-text-field>
+                      </v-col>
+                      <v-col cols="12" lg="10" md="10">
+                        <span style="color: #747474">لینک خارجی</span>
+                        <v-text-field rounded dense></v-text-field>
+                      </v-col>
+                    </v-row>
+                    <div class="d-flex justify-end">
+                      <v-btn color="primary" class="px-8 rounded-0">
+                        ذخیره
+                      </v-btn>
+                    </div>
                   </v-expansion-panel-content>
                 </v-expansion-panel>
               </v-expansion-panels>
@@ -34,7 +68,7 @@ export default Vue.extend({
   name: 'MainMenu',
   data() {
     return {
-      items: generateItems(5, (i: any) => ({ id: i, data: 'Draggable ' + i })),
+      items: generateItems(5, (i: any) => ({ id: i, data: 'منوی اصلی ' + i })),
     }
   },
   methods: {
@@ -47,10 +81,26 @@ export default Vue.extend({
 
 <style lang="scss" scoped>
 .draggable-item {
-  border: 2px solid #dad9d9;
   border-radius: 5px;
   margin-bottom: 2px;
   margin-top: 2px;
   cursor: default;
+}
+.expansion-header-title {
+  color: #747474;
+  font-size: 12px;
+  font-weight: 500;
+}
+.v-expansion-panels {
+  width: 40%;
+}
+.v-expansion-panel-header {
+  background-color: #f5f5f5;
+  border: 2px solid #dad9d9;
+  border-radius: 5px;
+}
+.v-expansion-panel-content {
+  background-color: #f5f5f5;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 }
 </style>
