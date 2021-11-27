089afd23c1284c0ca5a5387a7cb23f3bf3779f7e
Alireza Hosseini <alireza.hosseini7902@gmail.com>
2021-11-27 21:05:25 +0330

    start ProductsManagement page

---
+++ components/products/ProductsTableBox.vue
@@ -0,0 +1,80 @@
+<template>
+  <div style='padding-bottom: 250px' class='white ma-0 container__table'>
+    <div class='d-flex flex-column'>
+      <ProductsHeaderTable />
+      <v-divider class='mt-7'></v-divider>
+      <ProductsBodyTable />
+    </div>
+  </div>
+</template>
+
+<script>
+import UsersHeaderTable from '~/components/users/UsersHeaderTable'
+import UsersBodyTable from '~/components/users/UsersBodyTable'
+import ProductsHeaderTable from './ProductsHeaderTable'
+import ProductsBodyTable from './ProductsBodyTable'
+
+export default {
+  name: 'ProductsTableBox',
+  components: { ProductsBodyTable, ProductsHeaderTable, UsersBodyTable, UsersHeaderTable },
+  data() {
+    return {
+      images: {
+        topBig: '/images/settings-sample.jpg'
+      },
+      items: [
+        {
+          fullName: 'حسین دانش فر',
+          registeryDate: '1400/12/12',
+          category: 'کوزه، سفال'
+        },
+        {
+          fullName: 'حسین دانش فر',
+          registeryDate: '1400/12/12',
+          category: 'کوزه، سفال'
+        },
+        {
+          fullName: 'حسین دانش فر',
+          registeryDate: '1400/12/12',
+          category: 'کوزه، سفال'
+        },
+        {
+          fullName: 'حسین دانش فر',
+          registeryDate: '1400/12/12',
+          category: 'کوزه، سفال'
+        },
+        {
+          fullName: 'حسین دانش فر',
+          registeryDate: '1400/12/12',
+          category: 'کوزه، سفال'
+        }
+      ],
+      headers: [
+        {
+          text: 'نام و نام‌خانوادگی',
+          value: 'fullName',
+          width: '100px'
+        },
+        {
+          text: 'تاریخ عضویت',
+          value: 'registeryDate',
+          width: '100px'
+        },
+        { text: 'دسته', value: 'category', width: '200px' },
+        { text: 'عملیات', value: 'actions', width: '200px' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+
+.container__table {
+  height: auto;
+  @media screen and (max-width: 600px) {
+    width: 1200px;
+  }
+
+}
+</style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,13 +1,250 @@
 <template>
-
+  <div>
+    <TLFContainer>
+      <v-row style='column-gap: 1rem; row-gap: 1rem' class='' align='center'>
+        <span class='title mr-3 mr-sm-0'>مدیریت محصولات</span>
+        <v-btn width='230' height='35' class='mr-5 d-none d-md-inline-block' rounded color='primary'>
+          <span class='text-btn'>ذخیره اطلاعات محصول</span>
+        </v-btn>
+      </v-row>
+      <v-row style='column-gap: 0.5rem;row-gap: 1rem;' class='mt-15' align='center'>
+        <v-col cols='12' sm='6' md='4'>
+          <span class='white--text' style='font-size: 14px; font-weight: 400'>عنوان محصول</span>
+          <v-text-field
+            height='40'
+            class='elevation-3 mt-3'
+            hide-details
+            solo
+            placeholder='عنوان محصول'></v-text-field>
+        </v-col>
+        <v-col cols='12' sm='5' md='3'>
+          <span class='white--text' style='font-size: 14px; font-weight: 400'>عکس شاخص محصولات</span>
+          <label style='width: 200px; background-color: #2D9DCD;'
+                 class='mt-3 d-flex justify-center align-center rounded-xl py-2 upload'>
+            <v-icon color='#fff' size='10'>fa-plus</v-icon>
+            <span class='white--text mr-1' style='font-size: 14px; font-weight: 400'>عکس های محصول</span>
+            <input @input='addFiles' class='d-none' type='file' accept='image/*'>
+          </label>
+        </v-col>
+        <v-col v-for='(file, index) in files' :key='index' class='pa-0' md='1' cols='2' style='width: 100px; height: 50px'>
+          <img style='width: 100%; height: 100%; object-fit: cover' :src='file' alt=''>
+        </v-col>
+      </v-row>
+      <v-row style='column-gap: 0.5rem;row-gap: 1rem;' class='mt-5' align='start'>
+        <v-col cols='12' sm='4'>
+          <span class='white--text' style='font-size: 14px; font-weight: 400'>محتوا کوتاه محصول</span>
+          <v-textarea
+            class='elevation-3 mt-3'
+            solo
+            hide-details
+            placeholder='محتوا کوتاه محصول'></v-textarea>
+        </v-col>
+        <v-col cols='5' sm='3'>
+          <span class='white--text' style='font-size: 14px; font-weight: 400'>انتخاب امتیاز</span>
+          <v-select
+            class='elevation-3 mt-3'
+            :items='rateItems'
+            append-icon='fa-chevron-down'
+            hide-details
+            solo
+          ></v-select>
+        </v-col>
+        <v-col cols='5' sm='3'>
+          <span class='white--text' style='font-size: 14px; font-weight: 400'>انتخاب کارگاه</span>
+          <v-select
+            class='elevation-3 mt-3'
+            :items='workShopItems'
+            hide-details
+            append-icon='fa-chevron-down'
+            solo
+          ></v-select>
+        </v-col>
+      </v-row>
+      <v-row style='column-gap: 0.5rem;row-gap: 1rem;' class='mt-5' align='start'>
+        <v-col md='4' sm='6' cols='12'>
+          <v-card rounded='xl' class='py-4 px-5'>
+            <v-text-field
+              class='mt-4'
+              append-icon='fas fa-search'
+              style='min-height: 36px !important;'
+              filled
+              rounded
+              dense
+              placeholder='جستجوی دسته'
+              hide-details />
+            <v-virtual-scroll
+              class='mt-4 tlf-v-scroll'
+              :items='categories'
+              :item-height='60'
+              height='400'
+            >
+              <template #default='{item}'>
+                <div class='px-4 py-4 tlf-role-item' :class='{activate: item.isSelect}'
+                     @click='myToggleFunction($event, item.id)'>
+                  {{ item.title }}
+                </div>
+              </template>
+            </v-virtual-scroll>
+          </v-card>
+        </v-col>
+        <v-col md='6' sm='4' cols='12'>
+          <span class='white--text' style='font-size: 14px; font-weight: 400'>دسته های محصول</span>
+          <v-row class='mt-3'>
+            <v-col v-for='select in categories' :key='select.id'
+                   v-if='select.isSelect'
+                   cols='4'>
+              <v-btn @click='unSelectCategories($event, select.id)' rounded color='warning'>
+                <span style='color: #FFFFFF'>{{ select.title }}</span>
+                <div class='mr-3'
+                     style='border-radius: 50%;
+                     background-color: #000000; padding: 1px 4px'>
+                  <v-icon size='15' color='#ffffff'>fa-times</v-icon>
+                </div>
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </TLFContainer>
+    <ProductsTableBox class='mt-10 mb-10 d-none d-md-block' />
+    <v-btn width='230' height='35' class='mr-5 mb-3 mt-3 d-inline-block d-md-none' rounded color='primary'>
+      <span class='text-btn'>ذخیره اطلاعات محصول</span>
+    </v-btn>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'index'
-}
+import Vue from 'vue'
+import TLFContainer from '../../components/utilities/TLF-Container'
+import UsersTableBox from '../../components/users/UsersTableBox'
+import ProductsTableBox from '../../components/products/ProductsTableBox'
+
+
+export default Vue.extend({
+  name: 'index',
+  components: { ProductsTableBox, UsersTableBox, TLFContainer },
+  data() {
+    return {
+      rateItems: ['1 امتیاز', '2 امتیاز', '3 امتیاز'],
+      workShopItems: ['کارگاه 1', 'کارگاه 2', 'کارگاه 3'],
+      isActive: false,
+      select: 0,
+      categories: [
+        {
+          id: 1,
+          title: 'سفال',
+          isSelect: false
+        },
+        {
+          id: 2,
+          title: 'کوزه',
+          isSelect: false
+        }
+      ],
+      files: []
+    }
+  },
+  methods: {
+    myToggleFunction(event, id) {
+      const itemSelect = this.categories.find((item) => {
+        return item.id === id
+      })
+      itemSelect.isSelect = !itemSelect.isSelect
+    },
+    unSelectCategories(event, id) {
+      const itemSelect = this.categories.find((item) => {
+        return item.id === id
+      })
+      itemSelect.isSelect = false
+    },
+    addFiles(event) {
+      this.files.push(URL.createObjectURL(event.target.files[0]))
+      console.log(this.files)
+    }
+  }
+})
 </script>
 
-<style scoped>
+<style scoped lang='scss'>
+
+.title {
+  font-size: 1.6rem !important;
+  color: #fff !important;
+  font-weight: normal !important;
+  font-family: IranYekanFN, serif !important;
+}
+
+.text-btn {
+  font-size: 1rem !important;
+  color: #fff !important;
+  font-weight: 400 !important;
+  font-family: IranYekanFN, serif !important;
+}
+
+.text-number {
+  font-size: 1rem !important;
+  font-weight: 400 !important;
+  color: #000;
+  font-family: IranYekanFN, serif !important;
+}
+
+.upload {
+  @include transition();
+  cursor: pointer;
+
+  &:hover {
+    background-color: #258ca8 !important;
+  }
+}
+
+
+#plus {
+
+  min-height: 38px !important;
+  background: #2D9DCD !important;
+  border-radius: 76px !important;
+}
+
+.border-bt {
+  border-bottom: #F2F2F2 1px solid;
+  border-radius: 16px;
+  padding: 15px;
+  width: 100% !important;
+}
+
+.activate {
+  background-color: #2D9DCD !important;
+  color: white !important;
+}
+
+button {
+  width: 100%;
+}
+
+.tlf-v-scroll {
+  background-color: $near-white;
+  border-radius: 26px;
+}
+
+.tlf-role-item {
+  border-bottom: 1px solid #F2F2F2;
+  cursor: pointer;
+  @include transition();
+
+  &:hover {
+    background-color: #d8d7e0;
+  }
+}
+
+.tlf-role-item.selected {
+  background-color: $primary;
+  color: white;
+}
+
+.tlf-role-form-card-title {
+  font-size: 26px;
+  font-weight: 500;
+  color: $dark-grey;
+}
 
 </style>
