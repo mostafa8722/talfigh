eb9a0e6c571b123c02507ebe8382d900320a7731
Ali Raghebi <aliraghebiii@gmail.com>
2021-11-30 09:07:29 +0330

    fix conflict

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,133 @@
 <template>
-  <v-app class='container__app tlf-main' dark>
-    <Menu :items='items' v-if='!isMobile' />
-    <MenuMobile @heightMenu='setMarginMain' :items='items' v-if='isMobile' />
-    <v-main :style='{marginTop: marginTopMenuMobile + "px"}'>
-      <Nuxt />
+  <v-app dark>
+    <v-navigation-drawer
+      v-model='drawer'
+      :mini-variant='miniVariant'
+      clipped
+      right
+      fixed
+      app
+    >
+      <v-list>
+        <div v-for='(item, i) in items'
+             :key='i'>
+          <div v-if="item.type === 'divider'" class='px-8 py-4 tlf-menu-divider'>
+            <span>{{ item.title }}</span>
+          </div>
+
+          <v-list-item
+            v-else-if="item.type==='item'"
+            class='tlf-list-item'
+            :to='item.to'
+            router
+            exact
+          >
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title v-text='item.title' />
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-group v-else-if="item.type === 'group'" append-icon=''>
+            <template #activator>
+                <v-list-item-action>
+                  <v-icon>{{ item.icon }}</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title v-text='item.title' />
+                </v-list-item-content>
+            </template>
+
+            <v-list-item
+              v-for='(child,childIndex) in item.children'
+              :key='childIndex'
+              class='tlf-list-item-child'
+              :to='child.to'
+              router
+              exact
+            >
+              <v-list-item-action class='mr-4'>
+                <v-icon>{{ child.icon }}</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title v-text='child.title' />
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+        </div>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      class='tlf-app-bar'
+      fixed
+      clipped-right
+      app
+      elevation='0'
+      height='80'
+    >
+      <v-app-bar-nav-icon @click.stop='drawer = !drawer' />
+
+      <v-img class='mr-8 ml-6' src='~/static/images/logo.png' max-width='36' max-height='28' />
+
+      <v-toolbar-title class='font-weight-bold ml-16' v-text='title' />
+
+      <v-avatar class='mr-5 ml-4'>
+        <v-icon size='36'>fas fa-user-circle</v-icon>
+      </v-avatar>
+
+      <span class='font-weight-bold'>
+        الیاس ملک پور
+      </span>
+
+      <span class='mr-8'>
+        09027777254
+      </span>
+
+      <v-spacer />
+
+      <v-btn text>
+        <span>جدول پاداش</span>
+        <v-icon class='mr-4'>fas fa-money-bill-wave</v-icon>
+      </v-btn>
+
+      <v-btn class='mr-16' text>
+        <span>تیکت</span>
+        <v-icon class='mr-4'>fas fa-pen-square</v-icon>
+      </v-btn>
+
+      <v-btn class='mr-16' text>
+        <span class='ml-4'>پیام‌ها</span>
+        <v-badge content='1' color='red lighten-2' left>
+          <v-icon>far fa-envelope</v-icon>
+        </v-badge>
+      </v-btn>
+
+      <v-btn class='mr-16' text>
+        <span class='ml-4'>پشتیبانی</span>
+        <v-badge content='2' color='red lighten-2' left>
+          <v-icon>fas fa-user-edit</v-icon>
+        </v-badge>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main class='tlf-main'>
+
+        <Nuxt />
+
     </v-main>
   </v-app>
 </template>
 
 <script lang='ts'>
 import Vue from 'vue'
-import Menu from '~/components/menu/Menu.vue'
-import MenuMobile from '~/components/menu/Menu-Mobile.vue'
 
 export default Vue.extend({
-  components: { MenuMobile, Menu },
   data() {
     return {
+      drawer: true,
       items: [
         {
           type: 'divider',
@@ -24,10 +135,9 @@ export default Vue.extend({
         },
         {
           type: 'item',
-          icon: 'fa-shopping-cart',
+          icon: 'fa-user-circle',
           title: 'محصولات',
-          to: '/products',
-          caption: '5 مورد'
+          to: '/'
         },
         {
           type: 'divider',
@@ -35,19 +145,13 @@ export default Vue.extend({
         },
         {
           type: 'item',
-          icon: 'fa-user-edit',
-          title: 'نقش‌ ها',
+          icon: 'fa-user-circle',
+          title: 'نقش‌ها',
           to: '/roles'
         },
         {
-          type: 'item',
-          icon: 'fa-users',
-          title: 'کاربران',
-          to: '/users'
-        },
-        {
           type: 'group',
-          icon: 'fa-user-graduate',
+          icon: 'fa-user-circle',
           title: 'مدیران',
           children: [
             {
@@ -61,86 +165,44 @@ export default Vue.extend({
               to: '/'
             }
           ]
-        },
-        {
-          type: 'item',
-          icon: 'fa-th',
-          title: 'منو ناوبری',
-          to: '/main-menu'
-        },
-        {
-          type: 'divider',
-          title: 'مدیریت صفحه ها'
-        },
-        {
-          type: 'item',
-          icon: 'fa-pen',
-          title: 'صفحه سازی',
-          to: '/custom'
-        },
-        {
-          type: 'item',
-          icon: 'fa-th',
-          title: 'منو ناوبری',
-          to: '/custom'
-        },
-        {
-          type: 'item',
-          icon: 'fa-edit',
-          title: 'تنظیمات سایت',
-          to: '/settings/home-settings',
-          caption: '5 مورد'
-        },
-        {
-          type: 'item',
-          icon: 'fa-edit',
-          title: 'تنظیمات سیستمی',
-          to: '/settings/home-settings'
-        },
-        {
-          type: 'item',
-          icon: 'fa-edit',
-          title: 'تنظیمات صفحه اصلی',
-          to: '/settings/system-settings'
         }
       ],
-      isMobile: false,
-      marginTopMenuMobile: 0,
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'پنل تلفیق هنر'
     }
-  },
-  methods: {
-    setMarginMain(number: any){
-      this.marginTopMenuMobile = number
-    }
-  },
-  mounted() {
-    let $ = this
-
-    window.addEventListener('resize', () => {
-      this.isMobile = window.innerWidth < 960
-      if (!this.isMobile){
-        this.marginTopMenuMobile = 0
-      }
-    })
-
-    if (!this.isMobile){
-      this.marginTopMenuMobile = 0
-    }
-    this.isMobile = window.innerWidth < 960
   }
 })
 </script>
 
 <style lang='scss' scoped>
-@import '~vuetify/src/styles/styles.sass';
-
-.tlf-main {
-  background: url("static/images/background.jpg") 50% 0;
+.tlf-app-bar {
+  background: linear-gradient(90.91deg, #F0FBFF -2.31%, #FFF7F0 110.95%);
+  color: $dark-grey;
 }
 
-//@media #{map-get($display-breakpoints, 'sm-and-down')}{
-//  .tlf-main {
-//    background: url("static/images/background.jpg") 50% 0;
-//  }
-//}
+.tlf-app-bar .v-btn {
+  color: $dark-grey;
+}
+
+.tlf-main {
+  background: linear-gradient(117.81deg, #FFB97A -18.78%, #00B2FF 126.29%);
+}
+
+.tlf-menu-divider {
+  font-size: 16px;
+  font-weight: 300;
+  color: $lighter-grey;
+}
+
+.tlf-list-item{
+  font-weight: bold;
+  color: $dark-grey;
+}
+
+.tlf-list-item-child{
+  font-weight: 400;
+  color: $dark-grey;
+}
 </style>
---
+++ pages/settings/site.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="py-10 px-10">
+    <div class="d-flex">
+      <h1 class="tlf-title ml-16">تنظیمات سایت</h1>
+      <v-btn class="rounded-xl mr-16" color="primary">ذخیره محتویات</v-btn>
+    </div>
+
+    <div class="mt-10">
+      <p class="tlf-subtitle">عنوان وبسایت - page title</p>
+      <v-row>
+        <v-col cols="3">
+          <v-text-field class="rounded-lg"
+                        background-color="white"
+                        placeholder="تلفیق هنر"
+                        solo
+                        filled
+                        dense
+                        outlined/>
+        </v-col>
+      </v-row>
+    </div>
+
+    <div class="mt-4">
+      <p class="tlf-subtitle">لینک شبکه‌های اجتماعی</p>
+      <v-row>
+        <v-col cols="3" class="d-flex">
+          <v-icon size="36" color="white">fab fa-instagram</v-icon>
+          <v-text-field class="rounded-lg mr-6"
+                        background-color="white"
+                        placeholder="تلفیق هنر"
+                        solo
+                        filled
+                        hide-details
+                        dense
+                        outlined/>
+        </v-col>
+
+        <v-col cols="3" class="mr-10 d-flex">
+          <v-icon size="36" color="white">fab fa-facebook-f</v-icon>
+          <v-text-field class="rounded-lg mr-6"
+                        background-color="white"
+                        placeholder="تلفیق هنر"
+                        solo
+                        filled
+                        hide-details
+                        dense
+                        outlined/>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="3" class="d-flex">
+          <v-icon size="36" color="white">fab fa-whatsapp</v-icon>
+          <v-text-field class="rounded-lg mr-6"
+                        background-color="white"
+                        placeholder="تلفیق هنر"
+                        solo
+                        filled
+                        hide-details
+                        dense
+                        outlined/>
+        </v-col>
+
+        <v-col cols="3" class="mr-10 d-flex">
+          <v-icon size="36" color="white">fab fa-google-plus-g</v-icon>
+          <v-text-field class="rounded-lg mr-6"
+                        background-color="white"
+                        placeholder="تلفیق هنر"
+                        solo
+                        filled
+                        hide-details
+                        dense
+                        outlined/>
+        </v-col>
+      </v-row>
+
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: "Site"
+})
+</script>
+
+<style scoped>
+.tlf-title {
+  font-weight: 500;
+  font-size: 1.6rem;
+  color: white;
+}
+
+.tlf-subtitle {
+  font-weight: 700;
+  font-size: 0.9rem;
+  color: white;
+}
+</style>
