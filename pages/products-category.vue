9ed48c2402c4590ab170dd9902eb1711e96634c8
Hossein <daneshfar.hossein7@gmail.com>
2021-12-06 22:11:14 +0330

    fix

--- pages/products-category.vue
+++ pages/products-category.vue
@@ -189,7 +189,7 @@ export default {
   data() {
     return {
       selectedCategory: {},
-      editCategoryDialog: true,
+      editCategoryDialog: false,
       categories: [
         {
           id: 1,
